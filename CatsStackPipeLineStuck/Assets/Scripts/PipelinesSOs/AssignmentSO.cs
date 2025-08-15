00aadcb0e71c8404ef7896751be10f4cb7c31393
RonCohen47 <138518157+RonCohen47@users.noreply.github.com>
2025-08-15 18:27:39 +0300

    Working on assignment data

--- CatsStackPipeLineStuck/Assets/Scripts/PipelinesSOs/AssignmentSO.cs
+++ CatsStackPipeLineStuck/Assets/Scripts/PipelinesSOs/AssignmentSO.cs
@@ -3,7 +3,24 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AssignmentSO", menuName = "Scriptable Objects/AssignmentSO")]
 public class AssignmentSO : ScriptableObject
 {
+    
     //id
     public string AssignmentName;
     public Sprite Sprite;
+    public AssignmentType assignmentType;
+
+    public enum AssignmentType
+    {
+        Dev,
+        Art,
+        Sound
+    }
+
+    public enum PolishType
+    {
+        None,
+        Dev,
+        Art,
+        Sound
+    }
 }
