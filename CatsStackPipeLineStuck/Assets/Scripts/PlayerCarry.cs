924b125939d1dce54b674462a8a00c7297933219
danieldaniel128 <danielkaarur@gmail.com>
2025-08-15 05:01:31 +0300

    PlayerCarry

---
+++ CatsStackPipeLineStuck/Assets/Scripts/PlayerCarry.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerCarry : MonoBehaviour
+{
+    // carry generic assignment or ????. if its assignment, it will have assignmentDataSO which conains the id of the assignment.
+    //this way, the player can carry any assignment, and the assignment will be identified by its id when is used, with event of assignment acomplished.
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void TryPick()
+    {
+
+    }
+    void Pick()
+    {
+
+    }
+    void Drop()
+    {
+        //press
+
+        //hold
+
+    }
+
+}
